35ecd0ed755d49d4b1f690a374715d6de25ad763
nashyvant <nashyvant@gmail.com>
2024-06-21 15:27:17 -0400

    Opening dead ends in creating the ship layout

--- bot_ext_fire.py
+++ bot_ext_fire.py
@@ -2,7 +2,7 @@ import random
 def init():
     #d = input("Enter row or column size D?")
     #d = int(d)
-    d = 4
+    d = 5
     row, col = (d, d)
     ship_map = [[0 for i in range(col)] for j in range(row)]
     print(ship_map)
@@ -19,7 +19,7 @@ def create_ship_layout(ship_map):
     col = len(ship_map[0])
     total = 0
     exactly_one_open_neigbor = []
-    first_attempt = 'true'
+    
     while( 1 ):
         for i in range(row):
             for j in range(col):
@@ -43,13 +43,62 @@ def create_ship_layout(ship_map):
         rand_block_cell = random.randint(0, len(exactly_one_open_neigbor)-1)
         (rand_row, rand_col) = exactly_one_open_neigbor[rand_block_cell]
         ship_map[rand_row][rand_col] = 1
-        first_attempt = 'false'
         exactly_one_open_neigbor = []
+    return
+
+def open_dead_end(ship_map):
+    row = len(ship_map)
+    col = len(ship_map[0])
+    total = 0
+    closed_neighbor_map = {} 
+    for i in range(row):
+        for j in range(col):
+            if(ship_map[i][j] == 1):
+                neighbor_list = []
+                if(i>0):
+                    total += ship_map[i-1][j]
+                    if ship_map[i-1][j] == 0:
+                        neighbor_list.append((i-1, j))
+                if(j < col-1):
+                    total += ship_map[i][j+1]
+                    if ship_map[i][j+1] == 0:
+                        neighbor_list.append((i, j+1))
+                if(j > 0):
+                    total += ship_map[i][j-1]
+                    if ship_map[i][j-1] == 0:
+                        neighbor_list.append((i, j-1))
+                if(i < row-1):
+                    total += ship_map[i+1][j]
+                    if ship_map[i+1][j] == 0:
+                        neighbor_list.append((i+1, j))
+                if(total == 1):
+                    if(len(neighbor_list) > 0):
+                        closed_neighbor_map[(i,j)] = neighbor_list
+                total = 0
+                   
+    if(len(closed_neighbor_map) == 0):
+        return #we are all done
+    num_dead_ends = len(closed_neighbor_map)
+    num_open_dead_end = num_dead_ends/2
+    count = 0
+    for curr_neighbors in closed_neighbor_map :
+        r_open_cell = random.randint(0, len(closed_neighbor_map[curr_neighbors])-1)
+        row, col= closed_neighbor_map[curr_neighbors][r_open_cell]
+        #row = row_col[0]
+        #col = row_col[1]
+        ship_map[row][col] = 1
+        count +=1
+        if(count >= num_open_dead_end):
+            break
     return
 
 # init the 2D map of ship
 d, ship_map = init()
 create_ship_layout(ship_map)
+print("ship layout:")
+print(ship_map)
+open_dead_end(ship_map)
+print("ship layout after opening dead ends:")
 print(ship_map)
 
 
